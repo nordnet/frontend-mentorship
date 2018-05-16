7b15373ac0ab31900b77bd549b00a2135f4de1b7
Vladimir Starkov <iamstarkov@gmail.com>
2018-05-16 02:50:05 +0200

    build it

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,12 @@ const shell = require('shelljs');
 const watch = require('gulp-watch');
 const sequence = require('run-sequence');
 
-
-const additionalMenu = [
-  { text: 'Pure UI',          url: 'https://rauchg.com/2015/pure-ui' },
-  { text: 'Documentation',    url: 'https://reactjs.org/docs/' },
-  { text: 'PropTypes',        url: 'https://reactjs.org/docs/typechecking-with-proptypes.html' },
-  { text: 'Create react app', url: 'https://github.com/facebook/create-react-app/' },
-  { text: 'github',           url: 'https://github.com/user/repo' },
-];
-
 // prettier-ignore
 const talks = [
   '00-js-intro',
   '01-react-basics',
   '02-routing',
+  '03-async'
 ];
 
 gulp.task('pretalks', done => {
